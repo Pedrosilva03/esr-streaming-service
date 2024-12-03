a6507e383791e522b3b026e4fa83dc13d50219f6
Pedrosilva03 <pedroo.silva180@gmail.com>
2024-12-03 04:22:36 +0000

    Troca de portas TCP

--- src/utils/Ports.java
+++ src/utils/Ports.java
@@ -4,11 +4,11 @@ package utils;
  * Classe que guarda alguns valores de portas padrão
  */
 public class Ports {
-    public static final int DEFAULT_SERVER_PORT = 53350;
+    public static final int DEFAULT_SERVER_PORT = 33350;
 
-    public static final int DEFAULT_NODE_TCP_PORT = 53350;
+    public static final int DEFAULT_NODE_TCP_PORT = 33350;
     public static final int DEFAULT_NODE_UDP_PORT = 7088;
 
-    public static final int DEFAULT_CLIENT_TCP_PORT = 53350;
+    public static final int DEFAULT_CLIENT_TCP_PORT = 33350;
     public static final int DEFAULT_CLIENT_UDP_PORT = 7088;
 }
